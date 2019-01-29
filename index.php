3de59500821a088c3c1c428a569424323b2593f6
evanryk <evanryk@gmail.com>
2019-01-29 22:43:08 +0200

    added base ui theme

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<h2>{ BookingSauce }</h2>
+	</header>
+	<main>
+		<div class="logbox">
+			<h2>Ola!</h2>
+
+			<div class="inputs">
+				<div class="field">
+
+					<i class="fas fa-envelope"></i>
+					<input type="text" name="email" placeholder="email" autocomplete="off">
+
+				</div>
+				<div class="field">
+
+					<i class="fas fa-lock"></i>
+					<input type="password" name="password" placeholder="password">
+
+				</div>
+				<button type="submit" name="login">Sign In</button>
+			</div>
+
+			<p>
+				Don't have an account? <a href="">Register</a>.
+			</p>
+
+		</div>
+	</main>
+	<footer>
+		<h2>copyright &copy EVAN CHRISTIANS 2018</h2>
+	</footer>
+</body>
+</html>

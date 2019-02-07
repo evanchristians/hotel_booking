e25d75d3adfcfd52cfd6d2fe224e6613c2e26df9
evanryk <evanryk@gmail.com>
2019-02-07 16:55:36 +0200

    added control on booking form for no user logged in

--- book.php
+++ book.php
@@ -11,12 +11,31 @@
 <body>
   <header>
   <h2>HotelBooking</h2>
-  <h3>
-    Logged in as <?php echo $_SESSION['user']; ?> | &nbsp <a href="index.php">Sign out</a>
-  </h3>
+  <?php
+  if (isset($_SESSION['user'])) {
+    ?>
+    <h3>
+      Logged in as <?php echo $_SESSION['user']; ?> | &nbsp <a href="index.php">Sign out</a>
+    </h3>
+  </header>
+    <?php
+  } else {
+    ?>
+    <h3>
+      <a href="register.php">Create an account</a> &nbsp | &nbsp <a href="index.php">Sign in</a>
+    </h3>
   </header>
   <main>
+    <h1>Welcome to HotelBooking.com</h1>
+    <p class="hero_greeting">
+      To make a booking
+      <a href="index.php" class="cta">Sign in</a> or 
+      <a href="register.php" class="cta">Create an account</a>
+    </p>
   </main>
+    <?php
+  }
+  ?>
   <footer>
   </footer>
 </body>
